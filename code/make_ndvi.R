9163a162f501c3c6e459ea300b32d07c8dfc7b28
Shenali Fernando <shenali5678@gmail.com>
2025-09-09 16:27:09 +1000

    add covariate ndvi

---
+++ code/make_ndvi.R
@@ -0,0 +1,12 @@
+library(terra)
+library(sp)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(corrplot)
+library(car)
+
+#Load covariate data 
+covars <- read.csv("C:/Users/shena/Desktop/ausplots/ppjsdm_ausplots/data/site_covariates.csv")
+
+
